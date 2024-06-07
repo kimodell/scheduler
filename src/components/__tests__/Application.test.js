8bab041c1185009982bc40eeb7feb92c75d2dbde
Kim O'Dell <kimbo.odell@gmail.com>
2024-06-07 13:42:05 -0400

    Add intigration test check if the Application component mounted, initial data loaded and interview can be booked or canceled

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,41 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, getByText, prettyDOM, getAllByTestId, findByText, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { findByText } = render(<Application />);
+describe("Application", () => {
+  //Define asynchronous function using await
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { queryByText, getByText, findByText } = render(<Application />);
+    // Promise chain hidden using await
+    await findByText("Monday");
 
-  return findByText("Monday");
+    fireEvent.click(getByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+
+    await findByText(container, "Archie Cohen");
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
+  });
 });
+
